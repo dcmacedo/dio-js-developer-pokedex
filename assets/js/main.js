891c0231d6d1c0806d649d96cbaeb7211603c3a7
Danilo Cotrim Macedo <dcotrimmacedo@gmail.com>
2023-12-28 19:47:44 -0300

    :sparkles: feat: create link individual pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,15 +10,17 @@ function convertPokemonToLi(pokemon) {
         <li class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
+        
+            <a href="pokemon.html" target="_blank">
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
 
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
+                    <img src="${pokemon.photo}"
+                        alt="${pokemon.name}">
+                </div>
+            </a>
         </li>
     `
 }
